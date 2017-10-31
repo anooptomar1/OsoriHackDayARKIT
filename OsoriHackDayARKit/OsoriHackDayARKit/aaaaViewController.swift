c5c6dae8a29bdcb652a57ec5daa2827d35d519d6
seubseub <seub0730@gmail.com>
2017-10-31 22:59:53 +0900

    Add td third party library

---
+++ OsoriHackDayARKit/OsoriHackDayARKit/aaaaViewController.swift
@@ -0,0 +1,35 @@
+//
+//  aaaaViewController.swift
+//  OsoriHackDayARKit
+//
+//  Created by Macbook Pro retina on 2017. 10. 31..
+//  Copyright © 2017년 com. All rights reserved.
+//
+
+import UIKit
+
+class aaaaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OsoriHackDayARKit/Pods/Target Support Files/Pods-OsoriHackDayARKitUITests/Pods-OsoriHackDayARKitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OsoriHackDayARKitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_OsoriHackDayARKitUITests
+@end
